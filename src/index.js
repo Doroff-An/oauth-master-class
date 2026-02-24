f75ed7aebb30deb03cc762f19a1a173d8fb2d2e2
Анна <anna-dorofeeva.04@mail.ru>
2026-02-24 14:37:52 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,23 +2,19 @@ window.onload = () => {
   document.getElementById("button").onclick = () => {
     window.YaAuthSuggest.init(
       {
-        client_id: "c46f0c53093440c39f12eff95a9f2f93",
+        client_id: "5a69cedc023b42259de1450bcc22e56d",
         response_type: "token",
-        redirect_uri: "https://examplesite.com/suggest/token",
+        redirect_uri: "https://oauth-master-class-seven.vercel.app/token.html",
       },
-      "https://examplesite.com",
-      {
-        view: "button",
-        parentId: "buttonContainer",
-        buttonSize: "m",
-        buttonView: "main",
-        buttonTheme: "light",
-        buttonBorderRadius: "0",
-        buttonIcon: "ya",
-      }
+      "https://oauth-master-class-seven.vercel.app",
     )
-      .then(({ handler }) => handler())
-      .then((data) => console.log("Сообщение с токеном", data))
-      .catch((error) => console.log("Обработка ошибки", error));
-  };
+   .then(({ handler }) => handler())
+      .then(async (data) => {
+        const result = await fetchYandexData(data.access_token);
+
+        authorize(result);
+
+        console.log(result, data);
+      })
+      .catch((error) => console.log("Что-то пошло не так: ", error));
 };
