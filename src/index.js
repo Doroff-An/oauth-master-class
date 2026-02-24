8fa0ffbdddb49678e2d83c8abe90a2837e79658d
Анна <anna-dorofeeva.04@mail.ru>
2026-02-24 14:50:47 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-window.onload = () => {
-  document.getElementById("button").onclick = () => {
-    window.YaAuthSuggest.init(
+window.YaAuthSuggest.init(
       {
-        client_id: "y0__xDp4eydBxiziz4gw6yxvxZ6EG-ah2BjVHmai_XgzrHt3yj4jA",
+        client_id: "5a69cedc023b42259de1450bcc22e56d",
         response_type: "token",
         redirect_uri: "https://oauth-master-class-seven.vercel.app/token.html",
       },
       "https://oauth-master-class-seven.vercel.app",
+      {
+        parentId: "buttonContainer",
+        view: "button",
+        buttonTheme: "light",
+        buttonSize: "xs",
+        buttonBorderRadius: 20,
+      }
     )
-   .then(({ handler }) => handler())
+      .then(({ handler }) => handler())
       .then(async (data) => {
         const result = await fetchYandexData(data.access_token);
 
@@ -17,4 +22,3 @@ window.onload = () => {
         console.log(result, data);
       })
       .catch((error) => console.log("Что-то пошло не так: ", error));
-};
