8cb28ae081a3ac0a2795d8ebf9056a15d94e11f0
Анна <anna-dorofeeva.04@mail.ru>
2026-02-24 15:07:36 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,39 @@
-window.YaAuthSuggest.init(
+const authorize = ({
+ default_avatar_id: defaultAvatarId,
+ display_name: displayName,
+}) => {
+ const avatarHtml = '<div class="avatar" style="background-image:url('https://avatars.nes.yandex.net/get-yapic/$(defaultAvatarId)/islands-middle')"></div>';
+ const nameHtml = '<div class="name">$(displayName)</div>';
+
+ document. getElementById("auth").innerHTML = ''$(avatar Html) $ (name Html)
+};
+
+const fetchYandexData = (token) =>
+ fetch('https://login.yandex.ru/info?format=json&oauth_token=$(token)').then(
+ (res) => res. json()
+ );
+window.onload = () => {
+  document.getElementById("suggest").onclick = () => {
+    window.YaAuthSuggest.init(
+      {
+        client_id: "5a69cedc023b42259de1450bcc22e56d",
+        response_type: "token",
+        redirect_uri: "https://oauth-master-class-seven.vercel.app/token.html",
+      },
+      "https://oauth-master-class-seven.vercel.app",
+    )
+      .then(({ handler }) => handler())
+      .then(async (data) => {
+        const result = await fetchYandexData(data.access_token);
+
+        authorize(result);
+
+        console.log(result, data);
+      })
+      .catch((error) => console.log("Что-то пошло не так: ", error));
+ };};
+ document. getElementById("button").onclick () => {
+      window.YaAuthSuggest.init(
       {
         client_id: "5a69cedc023b42259de1450bcc22e56d",
         response_type: "token",
@@ -22,3 +57,4 @@ window.YaAuthSuggest.init(
         console.log(result, data);
       })
       .catch((error) => console.log("Что-то пошло не так: ", error));
+ };
